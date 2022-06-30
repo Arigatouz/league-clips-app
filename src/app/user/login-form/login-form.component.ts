798da481be7364ec176fe3b53b9b6c2ac5d3fb08
Ali-Gamal <aga@luftborn.com>
2022-06-30 22:42:58 +0200

    login

--- src/app/user/login-form/login-form.component.ts
+++ src/app/user/login-form/login-form.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login-form',
